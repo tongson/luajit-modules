be386538365850a4712781f196b3b59b446c9675
Eduardo Tongson <propolice@gmail.com>
2021-05-23 13:00:06 +0800

    One more test for toml.

--- tests/Ttoml.lua
+++ tests/Ttoml.lua
@@ -12,10 +12,20 @@ local string_1 = function()
 	T.is_table(t.list.songs)
 	T.expected("Dead")(t.list.songs[1]["name"])
 end
+local string_2 = function()
+	local s = [[
+		test = 1
+	]]
+	local t = toml.parse(s)
+	T.is_table(t)
+	T.expected(1)(t.test)
+end
 if included then
 	return function()
 		T["string 1"] = string_1
+		T["string 2"] = string_2
 	end
 else
 	T["string 1"] = string_1
+	T["string 2"] = string_2
 end
