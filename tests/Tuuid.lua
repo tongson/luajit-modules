b85f78fceb6e4c22ca30d53b055276b6d40149a4
Eduardo Tongson <propolice@gmail.com>
2021-05-23 12:21:20 +0800

    Removed hwaddr.

--- tests/Tuuid.lua
+++ tests/Tuuid.lua
@@ -18,16 +18,6 @@ local format_of_the_generated_uuid = function()
 	end
 end
 
-local hwaddr_parameter = function()
-	T.error_raised(uuid("12345678901")) -- too short
-	T.error_raised(uuid("123a4::xxyy;;590")) -- too short after clean
-	T.error_raised(uuid(true)) -- not a string
-	T.error_raised(uuid(123)) -- not a string
-	T.is_string(uuid("abcdefabcdef")) -- hex only
-	T.is_string(uuid("123456789012")) -- right size
-	T.is_string(uuid("1234567890123")) -- oversize
-end
-
 local randomseed_properly_limits_the_value = function()
 	bitsize = 32
 	T.equal(12345, uuid.randomseed(12345))
@@ -47,4 +37,21 @@ else
 	T["hwaddr parameter"] = hwaddr_parameter
 	T["uuid.randomseed() properly limits the value"] =
 		randomseed_properly_limits_the_value
+	local t = {}
+	for j = 1, 1e4 do
+		uuid.seed()
+		for i = 1, 1e6 do
+			local id = uuid()
+			if i == 1 then
+				print(id)
+			end
+			if t[id] then
+				print(id)
+				print(i)
+			end
+			assert(not t[id])
+			t[id] = true
+		end
+		print(j..'M')
+	end
 end
