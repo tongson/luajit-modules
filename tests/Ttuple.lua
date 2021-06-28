30ba8d6c9e2262f0dba9d4fb8ee605e185e4d4c1
Eduardo Tongson <propolice@gmail.com>
2021-06-28 19:22:40 +0800

    Deep comparison of tuples.

--- tests/Ttuple.lua
+++ tests/Ttuple.lua
@@ -284,6 +284,19 @@ local comparison_nil = function()
 	expect(true)(tuple() == tuple())
 	expect(true)(tuple(1, nil) == tuple(1, nil))
 	expect(false)(tuple(2, nil) == tuple(1, nil))
+	expect(false)(tuple(1, nil, { false }) == tuple(1, false, { false }))
+end
+local deep_comparison = function()
+	local tup1 = tuple(1, nil, { false })
+	local tup2 = tuple(1, nil, { false })
+	local util = require("util")
+	expect(true)(util.deepcompare(tup1, tup2, true))
+	local tup3 = tuple(1, true, { false })
+	local tup4 = tuple(1, false, { false })
+	expect(false)(util.deepcompare(tup3, tup4))
+	local tup5 = tuple(1, nil, { false })
+	local tup6 = tuple(1, false, { false })
+	expect(false)(util.deepcompare(tup5, tup6))
 end
 local multiplication = function()
 	local t1 = tuple("a", "z") * 2
@@ -380,8 +393,9 @@ if included then
 		T[":contents with nil"] = array_nil
 		T["Addition"] = addition
 		T["Addition with nil"] = addition_nil
-		T["Comparison"] = comparison
-		T["Comparison with nil"] = comparison_nil
+		T["Equality"] = comparison
+		T["Equality with nil"] = comparison_nil
+		T["Deep compare tuples"] = deep_comparison
 		T["Multiplication"] = multiplication
 		T["Multiplication with nils"] = multiplication_nil
 		T["<"] = less_than
@@ -409,8 +423,9 @@ else
 	T[":contents with nil"] = array_nil
 	T["Addition"] = addition
 	T["Addition with nil"] = addition_nil
-	T["Comparison"] = comparison
-	T["Comparison with nil"] = comparison_nil
+	T["Equality"] = comparison
+	T["Equality with nil"] = comparison_nil
+	T["Deep compare tuples"] = deep_comparison
 	T["Multiplication"] = multiplication
 	T["Multiplication with nils"] = multiplication_nil
 	T["<"] = less_than
